e658f739f0fdc2053e6aaac03f3158b679ec4d96
Damir <m.damir97@mail.ru>
2021-05-01 17:40:14 +0300

    Добавьте файлы проекта.

---
+++ ExamApp/DataM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamApp
+{
+    class DataM
+    {
+        public string Error { get; set; }
+        public IList<Logs> Logs { get; set; }
+        private IList<Logs> Sort(IList<Logs> logs, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+                return logs;
+            var pivotIndex = Split(logs, minIndex, maxIndex);
+            Sort(logs, minIndex, pivotIndex - 1);
+            Sort(logs, pivotIndex + 1, maxIndex);
+
+            return logs;
+        }
+        private int Split(IList<Logs> logs, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (logs[i].Created_at.Ticks < logs[maxIndex].Created_at.Ticks)
+                {
+                    pivot++;
+                    Swap(pivot, i);
+                }
+            }
+            pivot++;
+            Swap(pivot, maxIndex);
+            return pivot;
+        }
+        private void Swap(int x, int y)
+        {
+            var temp = this.Logs[x];
+            this.Logs[x] = this.Logs[y];
+            this.Logs[y] = temp;
+        }
+        public void Sort()
+        {
+            this.Logs = Sort(this.Logs, 0, this.Logs.Count - 1);
+        }
+    }
+    public class Logs
+    {
+        public DateTime Created_at { get; set; }
+        public string First_name { get; set; }
+        public string Message { get; set; }
+        public string Second_name { get; set; }
+        public string User_id { get; set; }
+    }
+}
---
+++ ExamApp/Program.cs
@@ -0,0 +1,44 @@
+﻿//.NET 5.0
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.IO;
+using SQLite;
+
+namespace ExamApp
+{
+    class Program
+    {
+        private static readonly string sqliteConnectionString = @"Logs.db";
+        
+        private static string GetData(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.ContentType = @"application/json;charset=""utf-8""";
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sending HTTP request..");
+            string data = GetData("http://www.dsdev.tech/logs/20210103");
+            Console.WriteLine("Done.\nParsing HTTP response..");
+            DataM logs = new DataM();
+            logs = JsonSerializer.Deserialize<DataM>(data, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            Console.WriteLine("Done.\nStarting sort..");
+            logs.Sort();
+            Console.WriteLine("Done\nInserting into db..");
+            SQLiteConnection db = new SQLiteConnection(sqliteConnectionString, false);
+            db.InsertAll(logs.Logs);
+            db.Close();
+            Console.WriteLine("Done.\nFinished all Jobs.\nPress Enter to exit");
+            _ = Console.ReadLine();
+            return;
+        }
+    }
+}
